6d13ccec797e7aa32259d93b4f79843fb67f1fef
kegcheck <65734212+kegcheck@users.noreply.github.com>
2025-01-26 21:51:16 -0600

    fix screen width

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import Feed from './components/Feed';
 function App() {
 
   return (
-    <div className="flex justify-center items-center w-screen flex-col p-4 min-h-screen mt-24 relative">
-      <Header />
-      <div className="max-w-2xl flex flex-col gap-4">
-        <VideoGenerator />
-        <Feed />
+  
+      <div className="flex justify-center items-center w-screen flex-col p-4 mt-24 relative">
+        <Header />
+        <div className="max-w-2xl flex flex-col gap-4">
+          <VideoGenerator />
+          <Feed />
+        </div>
       </div>
-    </div>
+
+
   )
 }
 
