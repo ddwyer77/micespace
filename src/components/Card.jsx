871ed61eeb165954fc9edbb421ec9fc6f2005b12
kegcheck <65734212+kegcheck@users.noreply.github.com>
2025-02-18 19:55:10 -0600

    fix white text on dark mode with white bg

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ imageUrl, name, isSelected, onClick, loading }) => {
         <div onClick={onClick}  className={`${isSelected ? 'outline outline-2 outline-primary' : ''} w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-white hover:cursor-pointer hover:outline hover:outline-2 hover:outline-primary ${loading ? 'pointer-events-none' : ''}`}>
             <img className="object-cover w-full md:h-56" src={imageUrl} alt="avatar" />
             <div className="py-5 text-center">
-                <a href="#" className="block md:text-xl font-bold text-gray-800 dark:text-white" tabIndex="0" role="link">{name}</a>
+                <a href="#" className="block md:text-xl font-bold text-gray-800" tabIndex="0" role="link">{name}</a>
             </div>
         </div>
     );
